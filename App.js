97ad98b341d2fc38ede7e83a50528868ce1784c5
romanusObialasor <robialasor5@gmail.com>
2021-05-19 04:09:35 +0100

    assignment

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './myCSS.css'
+
+const align=()=>{
+
+ return(
+
+<div className="main_div">
+      <div className="top_left">REACT.JS</div>
+      <div className="center">NODE.JS</div>
+      <div className="top_right">MONGO DB</div>
+      <div className="bottom_left">ANGULAR.JS</div>
+      <div className="bottom_right">ROMANUS</div>
+</div>
+)
+
+}
+
+export default align
